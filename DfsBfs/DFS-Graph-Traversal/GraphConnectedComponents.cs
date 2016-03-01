4b9e5dbf8bb12285064f5a2f3fb461b331d06457
Asparuh Krastev <asparuh.krastev@gmail.com>
2016-03-01 19:20:03 +0200

    DfsBfs

---
+++ DfsBfs/DFS-Graph-Traversal/GraphConnectedComponents.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class GraphConnectedComponents
+{
+    private static bool[] visited;
+    private static List<int>[] graph;
+
+    static void DFS(int node)
+    {
+        if (visited[node])
+        {
+            return;
+        }
+        visited[node] = true;
+        foreach (var child in graph[node])
+        {
+            DFS(child);
+        }
+        Console.Write(" {0}", node);
+    }
+
+    static void FindConnectedComponent()
+    {
+
+        visited = new bool[graph.Length];
+        for (int i = 0; i < graph.Length; i++)
+        {
+            if (visited[i])
+            {
+                continue;
+            }
+            Console.Write("Connected component:");
+            DFS(i);
+            Console.WriteLine();
+        }
+
+    }
+
+    public static void Main()
+    {
+        try {
+            var count = int.Parse(Console.ReadLine());
+            graph = new List<int>[count];
+            for (int i = 0; i < count; i++)
+            {
+                string[] vals = Console.ReadLine().Split();
+                graph[i] = new List<int>(vals.Length);
+                foreach (var val in vals)
+                {
+                    int v;
+                    if (int.TryParse(val, out v))
+                    {
+                        graph[i].Add(v);
+                    }
+                }
+            }
+            FindConnectedComponent();
+
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        Console.ReadLine();
+    }
+}
