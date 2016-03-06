0174c42fd0d7786018807e32068f58054cc394d0
Asparuh Krastev <a.krastev@up2.nl>
2016-03-06 19:50:42 +0200

    Finding root and round dance

--- TreeTraversal/TreeTraversal/Graph.cs
+++ TreeTraversal/TreeTraversal/Graph.cs
@@ -7,40 +7,131 @@ namespace TreeTraversal
 	public class Node<T>
 	{
 		public T Value { get; private set; }
-		public Node<T> Parent;
+		public List<Node<T>> Parents;
+		public List<Node<T>> Children;
 		public Node(T val)
 		{
 			Value = val;
+			Parents = new List<Node<T>> ();
+			Children = new List<Node<T>> ();
 		}
 	}
+
 	public class Graph<T, Node>
 	{
-		public int Count { get; private set; }
 		public Dictionary<T, Node<T>> Nodes;
+		protected static Dictionary<T, bool> visited;
 
-		public Graph (int count)
+		public Graph ()
 		{
-			Count = count;
-			Nodes = new Dictionary<T, Node<T>> (count);
+			Nodes = new Dictionary<T, Node<T>> ();
 		}
 
-		public void AddNode(Node<T> node)
-		{
-			if (Nodes.Count == Count) {
-				throw new OverflowException ("No more than " + Count + " nodes allowed.");
+		public void AddPair( T nodeVal, T parentVal ) {
+			Node<T> parent;
+			Node<T> node;
+
+			if (!Nodes.ContainsKey (parentVal)) {
+				parent = new Node<T> (parentVal);
+				Nodes [parentVal] = parent;
+			} else {
+				parent = Nodes [parentVal];
 			}
-			Nodes [node.Value] = node;
-			Count++;
+
+			if (!Nodes.ContainsKey (nodeVal)) {
+				node = new Node<T> (nodeVal);
+				Nodes [nodeVal] = node;
+			} else {
+				node = Nodes [nodeVal];
+			}
+			node.Parents.Add (parent);
+			parent.Children.Add (node);
+
 		}
 
 		public static bool IsRoot(Node<T> node)
 		{
-			return null == node.Parent;
+			return 0 == node.Parents.Count;
 		}
 
-		public Node<T> GetRoot()
+		public List<T> GetRoot()
 		{
-			return Nodes.Values.FirstOrDefault (node => IsRoot(node));
+			List<T> roots = new List<T>();
+			foreach (var node in Nodes.Values) {
+				if (IsRoot (node)) {
+					roots.Add (node.Value);
+				}
+			}
+			return roots;
+		}
+
+		public void PrintRoot()
+		{
+			var roots = GetRoot ();
+			switch (roots.Count) {
+			case 0:
+				Console.WriteLine ("No root!");
+				break;
+			case 1:
+				Console.Write ("The root is: {0}", string.Join(", ", GetRoot().ToArray()));
+				break;
+			default:
+				Console.WriteLine ("Multiple root nodes!");
+				break;
+			}
+		}
+
+		public static void GetFurthestNode( ref Node<T> FurthestNode, Node<T> parent, ref int maxDepth, int depth=0 )
+		{
+			if (visited.ContainsKey(parent.Value)) {
+				return;
+			}
+			visited [parent.Value] = true;
+			if( depth > maxDepth ) {
+				maxDepth = depth;
+				FurthestNode = parent;
+			}
+			foreach (var child in parent.Children) {
+				GetFurthestNode (ref FurthestNode, child, ref maxDepth, depth+1);
+			}
+		}
+
+	}
+
+	class LimitedGraph<T, Node>: Graph<T, Node> {
+		public int MaxCount { get; private set; }
+
+		public LimitedGraph (int max)
+		{
+			MaxCount = max;
+			Nodes = new Dictionary<T, Node<T>> (max);
+		}
+
+		public new void AddPair(T nodeVal, T parentVal)
+		{
+			if (Nodes.Count == MaxCount && (!Nodes.ContainsKey (nodeVal) || !Nodes.ContainsKey (parentVal))) {
+				throw new OverflowException ("No more than " + MaxCount + " nodes allowed.");
+			}
+			base.AddPair (nodeVal, parentVal);
+		}
+	}
+
+	class RoundGraph<T, Node>: Graph<T, Node> {
+		private T LeaderValue;
+
+		public RoundGraph(T leaderVal) {
+			LeaderValue = leaderVal;
+			Nodes = new Dictionary<T, Node<T>> ();
+		}
+
+		public int GetLongestDance()
+		{
+			visited = new Dictionary<T, bool> ();
+			int depth = 1;
+			var FurthestNode = Nodes [LeaderValue];
+			GetFurthestNode (ref FurthestNode, FurthestNode, ref depth, 1);
+
+			return depth;
 		}
 	}
 }
--- TreeTraversal/TreeTraversal/Program.cs
+++ TreeTraversal/TreeTraversal/Program.cs
@@ -7,31 +7,65 @@ namespace TreeTraversal
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Finding the root.");
-			Console.WriteLine ("Number of nodes:");
-			var cNodes = Console.ReadLine ();
-			Console.WriteLine ("Number of edges:");
-			var cEdges = Console.ReadLine ();
-			var graph = new Graph<int, Node<int>> (int.Parse(cNodes));
-			try{
-				for (var i=0; i<int.Parse(cEdges); i++) {
-					int[] pair = Console.ReadLine ().Split ().Select (h => int.Parse (h)).ToArray ();
-					var parent = new Node<int> (pair [0]);
-					var child = new Node<int> (pair [1]);
-					child.Parent = parent;
+			ConsoleKeyInfo key;
+			do {
+				Console.Clear();
+				Console.WriteLine("1 - Finding the root.");
+				Console.WriteLine("2 - Round dance.");
+				Console.WriteLine("q - Quit");
+				key = Console.ReadKey ();
+				Console.WriteLine();
+				switch( key.KeyChar) {
+					case '1':
+					Console.WriteLine ("Finding the root.");
+					Console.WriteLine ("Enter number of nodes:");
 					try {
-						graph.AddNode (parent);
-						graph.AddNode (child);
-					}
-					catch( OverflowException e) {
+						int cNodes = int.Parse (Console.ReadLine ());
+						Console.WriteLine ("Enter number of edges:");
+						int cEdges = int.Parse(Console.ReadLine ());
+						Graph<int, Node<int>> plainGraph = new LimitedGraph<int, Node<int>> (cNodes);
+						Console.WriteLine ("Enter nodes:");
+						for (var i=0; i<cEdges; i++) {
+							int[] pair = Console.ReadLine ().Split ().Select (h => int.Parse (h)).ToArray ();
+							try {
+								plainGraph.AddPair(pair[1], pair[0]);
+							} catch (OverflowException e) {
+								Console.WriteLine (e.Message);
+							}
+						}
+						plainGraph.PrintRoot();
+					} catch (FormatException e) {
 						Console.WriteLine (e.Message);
 					}
+					break;
+
+
+				case '2':
+					Console.WriteLine("Round dance.");
+					Console.WriteLine("Enter number of friendships:");
+					try {
+						int fCount = int.Parse(Console.ReadLine());
+						Console.WriteLine("Enter leader number:");
+						int leader = int.Parse(Console.ReadLine());
+						Console.WriteLine("Enter friendsips:");
+						RoundGraph<int, Node<int>> roundDance = new RoundGraph<int, Node<int>> (leader);
+						for( var i=0; i<fCount; i++) {
+							int[] pair = Console.ReadLine ().Split ().Select (h => int.Parse (h)).ToArray ();
+							roundDance.AddPair(pair[0], pair[1]);
+							roundDance.AddPair(pair[1], pair[0]);
+						}
+						Console.WriteLine("The longest round dance has {0} people", roundDance.GetLongestDance());
+					} catch (FormatException e) {
+						Console.WriteLine (e.Message);
+					}
+					catch (IndexOutOfRangeException e) {
+						Console.WriteLine (e.Message);
+					}
+					break;
 				}
-				Console.WriteLine("The root is: {0}", graph.GetRoot ());
-			}
-			catch(FormatException e) {
-				Console.WriteLine (e.Message);
-			}
+				Console.ReadKey();
+			}// do
+			while( 'q' != key.KeyChar );
 		}
 	}
 }
