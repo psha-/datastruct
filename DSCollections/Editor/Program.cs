958d463d3ab8793901822331760bff89af70163b
Asparuh Krastev <a.krastev@up2.nl>
2016-03-15 22:58:35 +0200

    Editor

--- DSCollections/Editor/Program.cs
+++ DSCollections/Editor/Program.cs
@@ -8,24 +8,36 @@ namespace Editor
 		public static void Main (string[] args)
 		{
 			var text = new BigList<string> ();
-			string command = " ";
+			string input = " ";
 			do {
-				command = Console.ReadLine();
-				var parts = command.Split(' ');
+				input = Console.ReadLine();
 
-				switch( parts[0]) {
-					case "INSERT":
-					text.Add(parts[1]);
+				string command;
+				string arguments;
+
+				if( -1 == input.IndexOf( ' ' )) {
+					command = input.Substring(0, input.IndexOf(' ')).ToLower();
+					arguments = input.Substring(input.IndexOf(' ')+1);
+				} else {
+					command = input;
+					arguments = " ";
+				}
+				switch( command ) {
+					case "append":
+					text.Add(arguments);
 					Console.WriteLine("OK");
 					break;
 
-					case "APPEND":
-					text.AddToFront(parts[1]);
+					case "insert":
+					var subText = arguments.Substring(0, arguments.LastIndexOf(' '));
+					var pos = int.Parse(arguments.Substring(arguments.LastIndexOf(' ')+1));
+					text.Insert(pos, subText);
 					Console.WriteLine("OK");
 
 					break;
 
-					case "DELETE":
+					case "delete":
+					var parts = arguments.Split( ' ' );
 					try {
 						text.RemoveRange(int.Parse(parts[1]), int.Parse(parts[2]));
 						Console.WriteLine("OK");
@@ -34,12 +46,13 @@ namespace Editor
 						Console.WriteLine("ERROR");
 					}
 					break;
-				case "PRINT":
-					Console.WriteLine (text);
+				case "print":
+					text.ForEach(Console.Write);
+					Console.WriteLine ();
 					break;
 				}
 
-			} while("END" != command);
+			} while("end" != input.ToLower());
 		}
 	}
 }
