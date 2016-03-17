212e49b4410726b8151d8288ce9434fa5a7fa27b
Asparuh Krastev <asparuh.krastev@gmail.com>
2016-03-17 22:07:25 +0200

    Avl tree

---
+++ AvlTree/AvlTreeLab/AvlTree.cs
@@ -0,0 +1,82 @@
+﻿namespace AvlTreeLab
+{
+    using System;
+    
+    public class AvlTree<T> where T : IComparable
+    {
+        private Node<T> root;
+
+        public int Count { get; private set; }
+        
+        public void Add(T item)
+        {
+            if( Contains(item))
+            {
+                return;
+            }
+            var newNode = new Node<T>(item);
+            if ( 0 == Count )
+            {
+                root = newNode;
+                Count++;
+                return;
+            }
+            var node = root;
+            while ( true )
+            {
+                if (item.CompareTo(node.Value) < 0)
+                {
+                    if (null == node.Left) {
+                        node.Left = newNode;
+                        newNode.Parent = node;
+                        break;
+                    }
+                    node = node.Left;
+                }
+                else
+                {
+                    if (null == node.Right)
+                    {
+                        node.Right = newNode;
+                        newNode.Parent = node;
+                        break;
+                    }
+                    node = node.Right;
+                }
+            }
+            Count++;
+            Rebalance(node);
+        }
+
+        private void Rebalance(Node<T> node)
+        {
+
+        }
+
+        public bool Contains(T item)
+        {
+            var node = root;
+            while (null != node)
+            {
+                if (0 == item.CompareTo(node.Value))
+                {
+                    return true;
+                }
+                else if (item.CompareTo(node.Value) < 0)
+                {
+                    node = node.Left;
+                }
+                else
+                {
+                    node = node.Right;
+                }
+            }
+            return false;
+        }
+
+        public void ForeachDfs(Action<int, T> action)
+        {
+
+        }
+    }
+}
---
+++ AvlTree/AvlTreeLab/Node.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace AvlTreeLab
+{
+    public class Node<T> where T : IComparable
+    {
+        public T Value { get; private set; }
+        public Node<T> Left { get; set;}
+        public Node<T> Right { get; set;}
+        public Node<T> Parent { get; set; }
+        public int BalanceFactor { get; set; }
+
+        public Node(T val)
+        {
+            Value = val;
+            BalanceFactor = 0;
+        }
+
+        private void UpdateCount()
+        {
+            if (null != Left)
+            {
+                BalanceFactor -= Left.BalanceFactor;
+            }
+            if (null != Right)
+            {
+                BalanceFactor += Right.BalanceFactor;
+            }
+        }
+    }
+}
+
